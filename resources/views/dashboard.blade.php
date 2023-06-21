972115abe17c159ecff4e707b984e0b17604f04d
Erick Briones <erickalan518@gmail.com>
2023-06-21 11:53:29 -0600

    Creando el muro

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,29 @@
 @extends('layouts.app')
 
 @section('title')
-    Cuenta
+    {{$user->name}} - {{ $user->username }}
 @endsection
 
 @section('main')
     <div class="flex justify-center">
-        <div class="w-full md:w-8/12 lg:w-1/2 md:flex">
-            <div class="md:w-8/12 lg:w-6/12 px-5">
-                <img src="{{ asset('img/usuario.svg') }}" alt="">
+        <div class="w-full md:w-8/12 lg:w-1/2 flex flex-col items-center md:flex-row">
+            <div class="w-8/12 lg:w-6/12 px-5">
+                <img src="{{ asset('img/usuario.svg') }}" alt="Imagen usuario">
             </div>
-            <div class="md:w-8/12 lg:w-6/12 px-5">
-                <p class="text-gray-700 text-2xl">{{ auth()->user()->username }}</p>
+            <div class="md:w-8/12 lg:w-6/12 px-5 flex flex-col items-center md:justify-center md:items-start py-10">
+                <p class="text-gray-700 text-2xl">{{ $user->username }}</p>
+                <p class="text-gray-800 text-sm mb-3 font-bold mt-5">
+                    0
+                    <span class="font-normal">Seguidores</span>
+                </p>
+                <p class="text-gray-800 text-sm mb-3 font-bold">
+                    0
+                    <span class="font-normal">Seguidos</span>
+                </p>
+                <p class="text-gray-800 text-sm mb-3 font-bold">
+                    0
+                    <span class="font-normal">Publicaciones</span>
+                </p>
             </div>
         </div>
     </div>
